fde0623ebb2d15a6eb1fbbf3abb4d2f6b5713776
RuiwenP2018 <rpan@sjnma.org>
2018-02-16 07:22:52 -0600

    update hw

--- hw4/p1.py
+++ hw4/p1.py
@@ -1,8 +1,15 @@
 #display the sentence word by word
 
+##s = raw_input ("pleace put in one sentence? ")
+
+##for word in s.split():
+
+##	print (word)
+
 s = raw_input ("pleace put in one sentence? ")
 
-for word in s.split():
+words = s.split()
 
- print (word)
+sentence = "\n".join(words)
 
+print (sentence)
--- hw4/test.py
+++ hw4/test.py
@@ -1,4 +1,18 @@
-string = "this is a string"
-ssplit = string.split()
-for word in ssplit:
-    print (word)
+name = raw_input('Please supply you whole name (first, middle, last): ')
+
+upper_name = name.upper()
+back_name = name[::-1]
+
+names = name.split()
+first = names[0]
+middle = names[1]
+last = names[2]
+
+center = len(middle)/2
+midfir = middle[0:center]
+midlas = middle[-center:]
+whole = (first + " " + midfir + "-BOB-" + midlas + " " + last)
+
+print (upper_name)
+print (back_name)
+print (whole)
