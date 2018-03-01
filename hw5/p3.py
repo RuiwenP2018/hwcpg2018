ffc7846db3ed441a9f122669c61987ce42928d7c
RuiwenP2018 <rpan@sjnma.org>
2018-02-28 20:53:11 -0600

    homework on hw5

--- hw5/p3.py
+++ hw5/p3.py
@@ -1,2 +1,17 @@
-sentence = raw_input ("Pleace enter a sentence and separate each letter with a (,)? ")
+sentence = raw_input ("Pleace enter a sentence and separate each letter with a comma? ")
 
+lower = sentence.lower()
+
+words = lower.split(',')
+
+numOfWords = len(words)
+
+if (numOfWords%2 == 1):
+
+	if ("llama" in words):
+
+		print (' '.join(words))
+
+elif ((numOfWords%2 == 1) or ((words[0]%2 == 1) and (words[1]%2 == 1) and (words[2]%2 == 1) and (words[3]%2 == 1) and (words[4]%2 == 1))):
+
+	print ("That is odd. ")
